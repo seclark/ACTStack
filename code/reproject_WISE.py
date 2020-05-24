aa2f02de6406cba613ebf7035b1d1cffba991516
seclark <susanclark19@gmail.com>
2020-05-24 01:38:20 -0400

    reproj

--- code/reproject_WISE.py
+++ code/reproject_WISE.py
@@ -39,10 +39,10 @@ if __name__ == "__main__":
     act220hdr2D["RADESYS"] = act220hdr["RADESYS"]
     
     print(wisehphdr)
-    print(act220hdr2D)
+    print(act220hdr2Dre)
     
     time0 = time.time()
-    reproj_wise, footprint = reproject_from_healpix((wisehpdata, wisehphdr), act220hdr2D, nested=False)
+    reproj_wise, footprint = reproject_from_healpix((wisehpdata, 'galactic'), act220hdr2D, nested=False)
     time1 = time.time()
     
     print("time = {} minutes".format((time1 - time0)/60.))
